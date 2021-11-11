3d1ed2b00198bcc70f7e7d17e5b7f5de9c0dd4ad
heinzaa <52783798+heinzaa@users.noreply.github.com>
2021-11-11 17:45:05 +0100

    -

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="Login">
+   <img alt="Vue logo" src="./assets/NYC.png">
+  
+
+
+
+
+
+
+</div>
+  
+</template>
+
+<script>
+export default {
+     name: 'Login',
+
+}
+</script>
+
+<style>
+
+</style>
